7a9647e5d05fdeb9b5be9596d76e96e273f9d6ae
Andy Ealovega <andyblarblar@gmail.com>
2020-05-31 23:45:59 -0400

    Added CLI arg parsing and download in serial.

--- IPSWdl/Program.cs
+++ IPSWdl/Program.cs
@@ -4,8 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Mono.Options;
 
 namespace IPSWdl
 {
@@ -17,28 +17,75 @@ namespace IPSWdl
 
         static async Task Main(string[] args)
         {
-            //the base directory the IPSW folder will be made in.
-            var pathToStoreFiles = args[0] ??= Directory.GetCurrentDirectory();
+            //CLI args
+            string pathToStoreFiles = null;
+            string searchTerm = null;
+            var showHelp = false;
 
+            var p = new OptionSet() {
+                "Usage: ipswdl [OPTIONS]",
+                "Downloads Apple IPSW firmware files.",
+                "By default, the latest IPSW is downloaded for every device available.",
+                "",
+                "Options:",
+                { "p|path=", "Specifies the {PATH} where files will be downloaded too. If not specified, the CWD is used.",
+                    p => pathToStoreFiles = p },
+                { "s|search=",
+                    "Only downloads for devices matching the {TERM}",
+                    s => searchTerm = s},
+                { "h|help",  "show this message and exit",
+                    v => showHelp = v != null },
+            };
+
+            try
+            { 
+                p.Parse(args);
+                //set path to CWD if not defined.
+                pathToStoreFiles ??= Directory.GetCurrentDirectory();
+            }
+            catch (OptionException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `ipswdl --help' for more information.");
+                return;
+            }
+
+            if (showHelp)
+            {
+                p.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            //Actual program
             Console.WriteLine("Getting devices...");
             var devices = await GetAllDevices();
             TotalCount = devices.Count;
             Console.WriteLine($"Got {TotalCount} devices!");
 
             var startTime = DateTime.Now;
-            Console.WriteLine("Starting Downloads... This will take a very long time. DO NOT CLOSE THE PROGRAM.");
+            Console.WriteLine("Starting Downloads... This may take a very long time. Any files still downloading when the program is closed will be corrupt.");
 
-            //Start a download of most recent IPSW for each device, and await all.
-            var dlTasks = new List<Task>();
-            foreach (var device in devices)
+            //Start a download of most recent IPSW for each device in serial. Downloading in parallel is better only in very high bandwidth scenarios. 
+            if (searchTerm is null)
             {
-                var firmware = await GetFirmwaresForDevice(device);
-                dlTasks.Add(DownloadMostRecentFirmware(firmware, pathToStoreFiles));
+                foreach (var device in devices)
+                {
+                    var firmware = await GetFirmwaresForDevice(device);
+                    await DownloadMostRecentFirmware(firmware, pathToStoreFiles);
+                }
+            }
+            else //only download based on search term if passed
+            {
+                TotalCount = devices.Count(d => d.name.Contains(searchTerm));
+                foreach (var device in devices.Where(d => d.name.Contains(searchTerm)))
+                {
+                    var firmware = await GetFirmwaresForDevice(device);
+                    await DownloadMostRecentFirmware(firmware, pathToStoreFiles);
+                }
             }
 
-            Task.WaitAll(dlTasks.ToArray());
-
-            Console.WriteLine($"Done in {(DateTime.Now - startTime).TotalMinutes} minutes");
+            Console.WriteLine($"Completed in {(DateTime.Now - startTime).TotalMinutes} minutes");
+            Console.WriteLine($"Press any key to quit...");
             Console.ReadKey();
         }
 
@@ -77,7 +124,10 @@ namespace IPSWdl
             await dlStream.CopyToAsync(fileStream);
 
             ++TotalDone;
-            Console.WriteLine($"Finished downloading {firmwareListing.name} {firmwareListing.firmwares[0].version} {(TotalDone/TotalCount) * 100}%");
+            Console.Write($"Finished downloading {firmwareListing.name} {firmwareListing.firmwares[0].version}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"                      {(TotalDone/TotalCount) * 100}% complete");
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
 
